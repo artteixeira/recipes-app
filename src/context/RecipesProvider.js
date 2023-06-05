80cf8fe13f85a0bec448a30c62b7e47c89a2b5f0
Arthur Teixeira <arthursony1@hotmail.com>
2023-06-05 16:39:59 -0300

    page login

--- src/context/RecipesProvider.js
+++ src/context/RecipesProvider.js
@@ -1,12 +1,19 @@
-// import React, { useState, useEffect } from 'react';
+import React, { useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 
 import RecipesContext from './RecipesContext';
 
 export default function RecipesProvider({ children }) {
+  const [login, setLogin] = useState({
+    email: '',
+    password: '',
+  });
+
+  const value = useMemo(() => ({ loginState: { login, setLogin } }), [login]);
+
   return (
     <RecipesContext.Provider
-      value=""
+      value={ value }
     >
       { children }
     </RecipesContext.Provider>
---
+++ src/pages/Login.jsx
@@ -0,0 +1,52 @@
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import validator from 'validator';
+
+import RecipesContext from '../context/RecipesContext';
+
+export default function Login() {
+  const { loginState } = useContext(RecipesContext);
+  const { login, setLogin } = loginState;
+
+  const history = useHistory();
+
+  const minLengthPassword = 6;
+  const buttonDisable = validator
+    .isEmail(login.email) && login.password.length >= minLengthPassword;
+
+  return (
+    <div>
+      <input
+        type="email"
+        name="email"
+        id="email"
+        data-testid="email-input"
+        value={ login.email }
+        onChange={ ({ target }) => {
+          setLogin({ ...login, email: target.value });
+        } }
+      />
+      <input
+        type="password"
+        name="password"
+        id="password"
+        data-testid="password-input"
+        value={ login.password }
+        onChange={ ({ target }) => {
+          setLogin({ ...login, password: target.value });
+        } }
+      />
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={ buttonDisable }
+        onClick={ () => {
+          localStorage.setItem('user', JSON.stringify({ email: login.email }));
+          history.push('/recipes');
+        } }
+      >
+        Enter
+      </button>
+    </div>
+  );
+}
