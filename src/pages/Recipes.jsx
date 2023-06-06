014b98a178408db2623b08689a50c51c1588634f
Rafaela Plinta <ra.plinta@gmail.com>
2023-06-06 09:52:38 -0300

    Adiciona requisitos 16, 17 e 18 parcial

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
-import Header from '../components/Header';
+import { useHistory } from 'react-router-dom';
+// import Header from '../components/Header';
+// import RecipesContext from '../context/RecipesContext';
+import Meals from '../components/Meals';
+import Drinks from '../components/Drinks';
 
 function Recipes() {
+  // const { headerState } = useContext(RecipesContext);
+  const history = useHistory();
+  const { pathname } = history.location;
+  // useEffect(() => {
+  //   headerState.setHeader({
+  //     title: pathname === '/meals' ? 'Meals' : 'Drinks',
+  //     profile: true,
+  //     search: true,
+  //   });
+  // }, []);
   return (
-    <Header />
+    <div>
+      { pathname === '/meals'
+        ? (<Meals />)
+        : (<Drinks />)}
+    </div>
   );
 }
 
