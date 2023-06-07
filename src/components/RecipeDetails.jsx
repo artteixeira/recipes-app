43b19e63e042966ff7064522ff3e4756fcc19e91
Arthur Teixeira <arthursony1@hotmail.com>
2023-06-07 20:07:11 -0300

    requisito 24 e 25

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -12,8 +12,9 @@ function RecipeDetails(props) {
     recipeDetail,
     setSearchBarFilter,
   } = useContext(RecipesContext);
-  console.log();
+
   const type = match.path.includes('drink') ? 'drinks' : 'meals';
+
   useEffect(() => {
     setSearchBarFilter({
       type: '',
@@ -24,12 +25,67 @@ function RecipeDetails(props) {
 
   return (
     <div>
-      <h1>AAAAAAAAA</h1>
-      {recipeDetail && recipeDetail.map((element) => (
-        <div key={ element.idMeal || element.idDrink }>
-          <h1>{element.strMeal || element.strDrink }</h1>
-        </div>
-      ))}
+      {recipeDetail && recipeDetail.map((element) => {
+        const ingredientsList = [];
+        const measuresList = [];
+        const magicNumber = 20;
+        for (let i = 0; i <= magicNumber; i += 1) {
+          const ingredient = element[`strIngredient${i}`];
+          const measure = element[`strMeasure${i}`];
+
+          if (ingredient) ingredientsList.push(ingredient);
+          if (measure) measuresList.push(measure);
+        }
+        return (
+          <div key={ element.idMeal || element.idDrink }>
+            <h1 data-testid="recipe-title">
+              {element.strMeal || element.strDrink }
+            </h1>
+            <img
+              width="200"
+              data-testid="recipe-photo"
+              src={ element.strMealThumb || element.strDrinkThumb }
+              alt={ element.strMeal || element.strDrink }
+            />
+            <p data-testid="recipe-category">
+              {element.strAlcoholic || element.strCategory}
+            </p>
+            <div>
+              <p>Ingredients</p>
+              { ingredientsList.map((ingredient, index) => (
+                <div
+                  key={ index }
+                  data-testid={ `${index}-ingredient-name-and-measure` }
+                >
+                  <span>{ingredient}</span>
+                  <span>{' - '}</span>
+                  <span>{measuresList[index]}</span>
+                </div>
+              ))}
+
+            </div>
+            <p data-testid="instructions">{element.strInstructions}</p>
+            { element.strYoutube && (
+
+              <iframe
+                data-testid="video"
+                width="560"
+                height="315"
+                src={ element.strYoutube.replace('watch?v=', 'embed/') }
+                title="YouTube video player"
+                allow="accelerometer;
+                autoplay;
+                clipboard-write;
+                encrypted-media;
+                gyroscope;
+                picture-in-picture;
+                 web-share"
+                allowfullscreen
+              />
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 }
