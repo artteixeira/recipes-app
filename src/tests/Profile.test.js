7b9bebb0093dd71c392f4c8b66ce5ea37d14b0e3
paulodelpra <paulodelpra@gmail.com>
2023-06-15 02:37:01 -0300

    testes profile 100%

--- src/tests/Profile.test.js
+++ src/tests/Profile.test.js
@@ -5,21 +5,7 @@ import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './utils/renderWithRouter';
 
-beforeEach(() => {
-  const user = { email: 'alguem@alguem.com' };
-  localStorage.setItem('user', JSON.stringify(user));
-  jest.spyOn(Object.getPrototypeOf(global.localStorage), 'getItem')
-    .mockReturnValue(JSON.stringify(user));
-});
-
 describe('Testes do componente Profile', () => {
-  test('Testa o e-mail recebido do LocalStorage', async () => {
-    renderWithRouter(<App />, '/profile');
-
-    await screen.findByTestId('profile-email');
-
-    expect(localStorage.getItem).toHaveBeenCalled();
-  });
   test('Testa se ao clicar no botão "Logout" o usuário é rederenciado para a rota "/"', () => {
     const { history } = renderWithRouter(<App />, '/profile');
 
@@ -32,5 +18,31 @@ describe('Testes do componente Profile', () => {
     });
 
     expect(history.location.pathname).toBe('/');
+  });
+  test('Testa se ao clicar no botão " Done Recipes" o usuário é rederenciado para a rota "/done-recipes"', () => {
+    const { history } = renderWithRouter(<App />, '/profile');
+
+    const doneRecipesBtn = screen.getByRole('button', {
+      name: /Done Recipes/i,
+    });
+
+    act(() => {
+      userEvent.click(doneRecipesBtn);
+    });
+
+    expect(history.location.pathname).toBe('/done-recipes');
+  });
+  test('Testa se ao clicar no botão " Favorite Recipes" o usuário é rederenciado para a rota "/done-recipes"', () => {
+    const { history } = renderWithRouter(<App />, '/profile');
+
+    const doneRecipesBtn = screen.getByRole('button', {
+      name: /Favorite Recipes/i,
+    });
+
+    act(() => {
+      userEvent.click(doneRecipesBtn);
+    });
+
+    expect(history.location.pathname).toBe('/favorite-recipes');
   });
 });
