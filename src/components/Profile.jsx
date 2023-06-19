ae9192864d8be6579d2e71e00ef22d0bbe7fbf3e
Rafaela Plinta <ra.plinta@gmail.com>
2023-06-19 10:37:18 -0300

    Adiciona css Profile pronto

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -22,35 +22,41 @@ function Profile() {
   return (
     <div>
       <Header />
-      <main>
-        <h1
-          data-testid="profile-email"
-        >
-          {email}
-        </h1>
-        <button
-          data-testid="profile-done-btn"
-          onClick={ () => history.push('/done-recipes') }
-        >
-          Done Recipes
-        </button>
+      <main className="profile-container">
+        <div className="btn-container">
+          <h1
+            className="profile-email"
+            data-testid="profile-email"
+          >
+            {email}
+          </h1>
+          <button
+            data-testid="profile-done-btn"
+            className="profile-btn"
+            onClick={ () => history.push('/done-recipes') }
+          >
+            Done Recipes
+          </button>
 
-        <button
-          data-testid="profile-favorite-btn"
-          onClick={ () => history.push('/favorite-recipes') }
-        >
-          Favorite Recipes
-        </button>
+          <button
+            data-testid="profile-favorite-btn"
+            className="profile-btn"
+            onClick={ () => history.push('/favorite-recipes') }
+          >
+            Favorite Recipes
+          </button>
 
-        <button
-          data-testid="profile-logout-btn"
-          onClick={ () => {
-            localStorage.clear();
-            history.push('/');
-          } }
-        >
-          Logout
-        </button>
+          <button
+            data-testid="profile-logout-btn"
+            className="profile-btn"
+            onClick={ () => {
+              localStorage.clear();
+              history.push('/');
+            } }
+          >
+            Logout
+          </button>
+        </div>
       </main>
       <Footer />
     </div>
